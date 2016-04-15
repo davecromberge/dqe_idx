bcf5ac569ddb44f21df257a829a1e0460753d2a6
Heinz N. Gies <heinz@licenser.net>
2016-04-15 16:35:27 +0200

    Add delete and add functions

--- src/dqe_idx.erl
+++ src/dqe_idx.erl
@@ -1,24 +1,69 @@
 -module(dqe_idx).
 
 %% API exports
--export([lookup/1]).
+-export([lookup/1, add/5, delete/5]).
 
 -type where() :: {binary(), binary()} |
                  {'and', where(), where()} |
                  {'or', where(), where()}.
--type query() :: dql:bm() |
-                 {binary(), [binary()], where()}.
 
--callback lookup(query()) -> {ok, {binary(), binary()}}.
+-type qry() :: dql:bm() |
+               {binary(), [binary()], where()}.
+
+-callback lookup(qry()) ->
+    {ok, {binary(), binary()}}.
+
+-callback add(Bucket::binary(),
+              Metric::[binary()],
+              LookupMetric::binary(),
+              TagKey::binary(),
+              TagValue::binary()) ->
+    {ok, {MetricIdx::non_neg_integer(), TagIdx::non_neg_integer()}}|
+    {error, Error::term()}.
+
+-callback delete(Bucket::binary(),
+                 Metric::[binary()],
+                 LookupMetric::binary(),
+                 TagKey::binary(),
+                 TagValue::binary()) ->
+    ok |
+    {error, Error::term()}.
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
--spec lookup(query()) -> {ok, [{binary(), binary()}]}.
+-spec lookup(qry()) ->
+                    {ok, [{binary(), binary()}]} |
+                    {error, Error::term()}.
 lookup(Query) ->
     Mod = idx_module(),
     Mod:lookup(Query).
+
+
+-spec add(Bucket::binary(),
+              Metric::[binary()],
+              LookupMetric::binary(),
+              TagKey::binary(),
+              TagValue::binary()) ->
+    {ok, {MetricIdx::non_neg_integer(), TagIdx::non_neg_integer()}}|
+    {error, Error::term()}.
+
+add(Bucket, Metric, LookupMetric, TagKey, TagValue) ->
+    Mod = idx_module(),
+    Mod:add(Bucket, Metric, LookupMetric, TagKey, TagValue).
+
+-spec delete(Bucket::binary(),
+                 Metric::[binary()],
+                 LookupMetric::binary(),
+                 TagKey::binary(),
+                 TagValue::binary()) ->
+    ok |
+    {error, Error::term()}.
+
+delete(Bucket, Metric, LookupMetric, TagKey, TagValue) ->
+    Mod = idx_module(),
+    Mod:delete(Bucket, Metric, LookupMetric, TagKey, TagValue).
 
 %%====================================================================
 %% Internal functions
